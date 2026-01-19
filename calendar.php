9357d9eda99794d4a877b9ab6d4c87b0136ef6ab
Wickramasinghe Arachchige Sajith Tharaka <129993532+Sajith22@users.noreply.github.com>
2026-01-19 19:13:39 +0530

    Updating the logic behind the appiontment fully

--- calendar.php
+++ calendar.php
@@ -64,11 +64,49 @@ if($_SERVER["REQUEST_METHOD"] ==="POST"&&($_POST["action"]??'')==="delete"){
 
     if($id){
         $stmt =$conn->prepare("DELETE FROM appointments WHERE id =?");
+
         $stmt->bind_param("i",$id);
         $stmt->execute();
         $stmt->close();
-        header("Location:" .$_SERVER["PHP_SELF"]."succes=3");
+        header("Location:" .$_SERVER["PHP_SELF"]."success=3");
         exit;
     }
 }
 
+# Success & Error Messages
+if(isset($_GET["success"])){
+    $successMsg = match ($_GET["success"]){
+        '1' => "✅Appointment added successfully",
+        '2' => "✅Appointment edited successfully",
+        '3' => "🗑️Appointment deleted successfully"
+    }
+}
+
+if(isset($_GET["error"])){
+    $errorMsg = '! Error occured. Please check your input.';
+}
+
+// Fetch All Appointments and Spread Over Data Range
+$result = $conn->query("SELECT * FROM appointments");
+
+if($result && $result->num_rows>0){
+    while($row =$result->fetch_assos()){
+        $start = new DateTime($row["start_date"]);
+        $end = new DateTime($row["end_date"]);
+
+        while($start <= $end){
+            $eventsFromDB[] = [
+                'id' => $row["id"],
+                'title' => "{$row['course_name'] - {$row['instructor_name']}}",
+                'date' => $start=>format("Y-m-d"),
+                "start" => $row["start_date"],
+                "end" => $row['end_date']
+            ];
+
+            $tart->modify('+1 day');
+        }
+    }
+    
+$conn ->close();
+
+?>
